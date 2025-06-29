c457b7229333a58e80b6b6b56c5edfad2c611b0d
ritsuhaaa <mharis830174@gmail.com>
2025-06-29 23:24:33 +0700

    feat: finish generation evaluation

--- evaluation/generation.py
+++ evaluation/generation.py
@@ -84,28 +84,28 @@ if __name__ == "__main__":
     test_cases = [
         {
             'query_id': 'Q1',
-            'query_text': 'waterproof smartphone under $500',
-            'reference': 'reference'  
+            'query_text': 'Apakah produk Xiaomi Redmi A2 merupakan barang baru?',
+            'reference': 'Produk ini merupakan HP Xiaomi Redmi A2 yang dijual dalam kondisi tidak baru, atau second, dan tersedia dalam beberapa grade kualitas seperti Grade A, B, C, dan X. Meskipun bekas, penjual memberikan garansi toko selama 7 hari dan menyertakan kelengkapan seperti dus original dan charger OEM.'  
         },
         {
             'query_id': 'Q2', 
-            'query_text': 'gaming laptop with RTX 4060',
-            'reference': 'reference'  
+            'query_text': 'Apakah produk Lenovo Yoga Slim 7i cocok untuk kuliah informatika?',
+            'reference': 'Laptop Lenovo Yoga Slim 7i ini sudah cukup cocok untuk menunjang kebutuhan kuliah di jurusan informatika. Performanya sangat mumpuni berkat prosesor terbaru Intel Core Ultra 7 155H yang memiliki 16 core, kapasitas RAM 16GB LPDDR5x yang sangat cepat dan 1TB SSD PCIe 4.0 memastikan aktivitas multitasking berjalan tanpa hambatan serta menyediakan ruang penyimpanan yang lebih dari cukup untuk proyek-proyek besar.'  
         },
         {
             'query_id': 'Q3',
-            'query_text': 'wireless headphones noise cancelling',
-            'reference': 'reference' 
+            'query_text': 'Jelaskan fitur lengkap dari produk Iphone 15',
+            'reference': 'Produk iPhone 15 menawarkan serangkaian fitur lengkap yang berpusat pada performa dan kualitas kamera. Produk ini hadir dengan layar OLED berukuran 6.1 inci dan menggunakan chip A16 Bionic. Kamera utamanya 48MP dengan dukungan pengambilan gambar resolusi tinggi, serta memiliki fitur video 4K. Tersedia dalam beberapa varian penyimpanan dan menggunakan konektor USB-C. Semua fitur ini berjalan pada sistem operasi iOS 17 dan produk ini disertai dengan garansi resmi iBox selama satu tahun.' 
         },
         {
             'query_id': 'Q4',
-            'query_text': 'wireless headphones noise cancelling',
-            'reference': 'reference' 
+            'query_text': 'Bagaimana cara penggunaan skincare Moisturizer SKINTIFIC?',
+            'reference': 'Gunakan moisturizer ini setelah membersihkan wajah dan memakai toner. Setelah wajah dibersihkan, aplikasikan moisturizer secara merata pada wajah dan leher. Oleskan secara merata ke wajah untuk menjaga kelembaban dan kesehatan kulit, serta gunakan pada pagi dan malam hari untuk hasil maksimal.' 
         },
         {
             'query_id': 'Q5',
-            'query_text': 'wireless headphones noise cancelling',
-            'reference': 'reference' 
+            'query_text': 'Berikan saya rekomendasi produk handphone dengan brand xiaomi',
+            'reference': 'Terdapat dua rekomendasi produk Xiaomi yang sangat berbeda yang dapat disesuaikan dengan kebutuhan Anda. Jika Anda mencari ponsel baru dengan performa tinggi, ruang penyimpanan sangat besar, dan jaminan jangka panjang, maka Xiaomi 14T adalah pilihan yang sangat direkomendasikan. Ponsel ini datang dalam kondisi baru (BNIB) dengan spesifikasi RAM 12 GB dan penyimpanan 512 GB. Sebaliknya, jika Anda memiliki anggaran yang sangat terbatas dan mencari ponsel untuk kebutuhan dasar, anda bisa mempertimbangkan Xiaomi Redmi A2 second. Produk ini adalah ponsel bekas dengan spesifikasi 3 GB RAM dan 32 GB penyimpanan yang dijual dalam kondisi Grade C atau memiliki lecet.' 
         }
     ]
 
