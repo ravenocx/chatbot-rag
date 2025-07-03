60d8e9c89110f71234d6013d8b51e926f0424720
ritsuhaaa <mharis830174@gmail.com>
2025-07-03 20:22:48 +0700

    feat: change inference 4-bit quant

--- rag/inference.py
+++ rag/inference.py
@@ -11,7 +11,10 @@ model_id = "meta-llama/Llama-3.3-70B-Instruct"
 bnb_config= BitsAndBytesConfig(
     load_in_4bit=True,
     llm_int8_threshold=6.0,
-    llm_int8_has_fp16_weight=False
+    llm_int8_has_fp16_weight=False, 
+    # bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_quant_type="nf4"
 )
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
